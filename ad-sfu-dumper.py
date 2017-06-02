b17c2893ac247de9feb1a076cb3ed68c11d75276
cnacorrea <carlos.nilton@gmail.com>
2017-06-02 14:40:05 -0300

    e necessario usar caminhos completos para rodar a partir do cron

--- ad-sfu-dumper.py
+++ ad-sfu-dumper.py
@@ -41,51 +41,49 @@ try:
             break
         else:
             if result_type == ldap.RES_SEARCH_ENTRY:
-				entrada = {}
-				if 'uidNumber' in result_data[0][1]:
-					entrada['login'] = result_data[0][1]['msSFU30Name'][0]
-					entrada['senha'] = result_data[0][1]['unixUserPassword'][0]
-					entrada['uid'] = result_data[0][1]['uidNumber'][0]
-					entrada['gid'] = result_data[0][1]['gidNumber'][0]
-					entrada['home'] = result_data[0][1]['unixHomeDirectory'][0]
-					entrada['shell'] = result_data[0][1]['loginShell'][0]
-					result_set.append(entrada)
+                                entrada = {}
+                                if 'uidNumber' in result_data[0][1]:
+                                        entrada['login'] = result_data[0][1]['msSFU30Name'][0]
+                                        entrada['senha'] = result_data[0][1]['unixUserPassword'][0]
+                                        entrada['uid'] = result_data[0][1]['uidNumber'][0]
+                                        entrada['gid'] = result_data[0][1]['gidNumber'][0]
+                                        entrada['home'] = result_data[0][1]['unixHomeDirectory'][0]
+                                        entrada['shell'] = result_data[0][1]['loginShell'][0]
+                                        result_set.append(entrada)
 except ldap.LDAPError, e:
-	print e
+        print e
 
 nome = []
-for linha in open('nomes-completos'):
+for linha in open('/etc/postfix/nomes-completos'):
     nome.append(tuple(linha.strip().split(',')))
 nome = dict(nome)
 
 for usuario in result_set:
-	try:
-		grp.getgrgid(usuario['gid'])
-	except KeyError:
-		print "ERRO: tentando atribuir grupo nao-existente (" + usuario['gid'] + ") ao usuario " + usuario['login']
-		continue
-	
-	try:
-		pwd.getpwnam(usuario['login'])
-	except KeyError:
-		try:
-			pwd.getpwuid(int(usuario['uid']))
-			print "ERRO: tentando atribuir ID ja existente (" + usuario['uid'] + ") ao usuario " + usuario['login']
-			continue
-		except KeyError:
-			if usuario['login'] in nome:
-				os.system("useradd -p " + usuario['senha'] + " -u " + usuario['uid'] +
-						" -g " + usuario['gid'] + " -d " + usuario['home'] + " -m " +
-						" -c " + "'" + nome[usuario['login']] + "'" +
-						" -s " + usuario['shell'] + " " + usuario['login'])
-			else:
-				os.system("useradd -p " + usuario['senha'] + " -u " + usuario['uid'] +
-						" -g " + usuario['gid'] + " -d " + usuario['home'] + " -m " +
-						" -s " + usuario['shell'] + " " + usuario['login'])
-			os.system("zarafa-admin -u " + usuario['login'] +
-			          " --enable-feature imap --qs 0 --qw 0 --qh 0 1>/dev/null 2>&1")
-			continue
-	
-	senha_atual = spwd.getspnam(usuario['login'])
-	if usuario['senha'] != spwd.getspnam(usuario['login']).sp_pwd:
-		os.system("usermod -p '" + usuario['senha'] + "' " + usuario['login'])
+        try:
+                grp.getgrgid(usuario['gid'])
+        except KeyError:
+                print "ERRO: tentando atribuir grupo nao-existente (" + usuario['gid'] + ") ao usuario " + usuario['login']
+                continue
+
+        try:
+                pwd.getpwnam(usuario['login'])
+        except KeyError:
+                try:
+                        pwd.getpwuid(int(usuario['uid']))
+                        print "ERRO: tentando atribuir ID ja existente (" + usuario['uid'] + ") ao usuario " + usuario['login']
+                        continue
+                except KeyError:
+                        if usuario['login'] in nome:
+                                os.system("/usr/sbin/useradd -p '" + usuario['senha'] + "' -u " + usuario['uid'] +
+                                                " -g " + usuario['gid'] + " -d " + usuario['home'] + " -m " +
+                                                " -c " + "'" + nome[usuario['login']] + "'" +
+                                                " -s " + usuario['shell'] + " " + usuario['login'])
+                        else:
+                                os.system("/usr/sbin/useradd -p '" + usuario['senha'] + "' -u " + usuario['uid'] + " -g " + usuario['gid'] + " -d " + usuario['home'] + " -m " + " -s " + usuario['shell'] + " " + usuario['login'])
+                        os.system("/usr/sbin/zarafa-admin -u " + usuario['login'] +
+                                  " --enable-feature imap --qs 0 --qw 0 --qh 0 1>/dev/null 2>&1")
+                        continue
+
+        senha_atual = spwd.getspnam(usuario['login'])
+        if usuario['senha'] != spwd.getspnam(usuario['login']).sp_pwd:
+                os.system("/usr/sbin/usermod -p '" + usuario['senha'] + "' " + usuario['login'])
